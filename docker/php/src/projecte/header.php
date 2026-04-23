8e0797840b86fc949f7dee952a825ff6a5b56c48
a25eduheryag <a25eduheryag@inspedralbes.cat>
2026-04-23 10:16:46 +0200

    Ordenar fitxers

---
+++ docker/php/src/projecte/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD de PHP Eduard Hernández</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  
+     <style>
+        body{
+            background-color: #f5f5f5;
+        }
+        .mi-caja{
+            padding:20px;
+            border-radius:10px;
+        }
+    </style>
+</head>
+<body>
+  <main class="container">
+    <h1>Aplicació d'incidencies Institut Pedralbes</h1>
