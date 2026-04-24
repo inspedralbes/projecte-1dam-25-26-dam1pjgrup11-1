256cf79f524320eb7dcf7a7b350eac5f4844b908
a25eduheryag <a25eduheryag@inspedralbes.cat>
2026-04-24 09:27:04 +0200

    Arregalar problema docker, crear tecnic.php

--- docker/php/index.php
+++ docker/php/index.php
@@ -10,7 +10,7 @@
      style="height: 80vh; width: 400px; background: white; border-radius: 15px; box-shadow: 0 0 15px rgba(0,0,0,0.1);">
     <h3>Qui ets?</h3>
     <a href="crear.php" class="btn btn-danger">Professor</a>
-    <a href="listar.php" class="btn btn-primary">Técnic</a>
+    <a href="tecnic.php" class="btn btn-primary">Técnic</a>
     <a href="admin.php" class="btn btn-dark">Admin</a>
 </div>
     
---
+++ docker/php/tecnic.php
@@ -0,0 +1,10 @@
+<?php include_once "header.php"?>
+
+<h1>Quin técnic es vol connectar?</h1>
+<h4>Posa el teu nom:</h4>
+<form action="">
+    <input type="text" name="nom" id="nom" value="Toni">
+    <input type="submit" value="Llistar les teves incidencies" class="btn btn-success btn-lg rounded mt-3 d-block">
+</form>
+
+<?php include_once "footer.php" ?>
