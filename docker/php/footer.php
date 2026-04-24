b06dcde524e49c1be2aa3b0760bce1d1942142ff
a25eduheryag <a25eduheryag@inspedralbes.cat>
2026-04-24 12:42:57 +0200

    feat: llistar total

--- docker/php/footer.php
+++ docker/php/footer.php
@@ -1,5 +1,5 @@
-<footer class="px-2 py-2 fixed-bottom bg-black">
-      <p class="text-center text-white">Victor Rios Quilez --- Eduard Hernández Yagüe</p>
-</footer>
-</body>
+<footer class="px-2 py-2 bg-black"> 
+      <p class="text-center text-white">Victor Rios Quilez --- Eduard Hernández Yagüe</p> 
+</footer> 
+</body> 
 </html>
