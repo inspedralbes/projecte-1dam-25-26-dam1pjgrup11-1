608a8aa4dafb6b4d13e50491377b1c62c55cc64b
a25vicrioqui <a25vicrioqui@inspedralbes.cat>
2026-04-24 09:38:39 +0200

    fix: bug

---
+++ docker/php/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicació incidencies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  
+     <style>
+        body {
+        background-color: #f5f5f5;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        }
+
+        
+        .titol h1{
+            justify-content: center;
+            text-align: center;
+        }
+        
+    </style>
+</head>
+<body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
